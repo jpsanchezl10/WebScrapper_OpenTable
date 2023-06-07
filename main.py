d65b2d9270e317f97d33558c173a3c6754054afb
jpsanchezl10 <jp.sanchezlorenzo@icloud.com>
2023-06-07 17:56:42 -0500

    adds per link

--- main.py
+++ main.py
@@ -2,45 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-def scrape_opentable():
-    user_agents = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36'
-    ]
-    URL = 'https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTC0MjC2MjBQ0lEyRpEBCpgABYDyxgYQeVMlKyMdJTOQKkM9IxNLEx1dCzM9C2NzS6CkBVA4wDUo2N_P0cczyjUoPjDUNSgSKGEJlFB2LEvMzElMykl1yy_ySSxJdcnMy0st8ssvByowBNobHQukgdalJeYUp9YCAHKAnWOlAAAA?originid=0e6cdce0-377b-4043-9a39-5ff316237575'
+
+def get_urls(URL, user_agents):
+
     user_agent = random.choice(user_agents)
     headers = {'User-Agent': user_agent}
     response = requests.get(URL, headers=headers)
-
-    results = []
     
     # Check if the request was successful
     if response.status_code == 200:
         # Create a BeautifulSoup object with the response content
         soup = BeautifulSoup(response.content, "html.parser")
 
-        # Find all the names
-        Name_results = soup.find_all("h6", class_="tfljo0SQq0JS3FOxpvxL")
-
-        # Find all the restaurant type/food
-        Style_results = soup.find_all("div", class_="u9ONW2kqbJZxSOxtuBJq")
-
-        #Find all price tags ( they have 4 and this code returns the missing so
-        #  if it returns ($) it means ($$$$(4) - $(1)) = $$$(3))
-        Price_results = soup.find_all("span", class_="AwKuroa75vy0Y4mLkyMr")
-
-        #latest review, 
-        
-        review_results = soup.find_all("span", class_="t9JcvSL3Bsj1lxMSi3pz b70r5ifNREO8o2AZafOi cpEOy_DPrbjR6hnlY0ub #2d333f")
-
-        #review number
-        review_number_results = soup.find_all("a", class_="z6Naf_ZXDiazhb9aJLoe")
-
-
-        ###############GETTING LINKS#############
+        ###############GETTING URLS#############
         # Find URLS
         URL_results = soup.find_all('a', href=True)
         # Create an empty set to store filtered href values
@@ -55,21 +29,81 @@ def scrape_opentable():
                 filtered_hrefs.add(url_before_question_mark)
     
         filtered_hrefs_list = list(filtered_hrefs)
-        ##############GETING LINKS################
+
 
         # Combine results
-        results = list(zip(Name_results, Style_results, Price_results,review_results,review_number_results, filtered_hrefs_list))
+    else:
+        print("Failed to retrieve URLs from OpenTable.")
+    
+    return filtered_hrefs_list
+
+
+###Get info from urls
+def scrape_opentable(URL, user_agents):
+
+    user_agent = random.choice(user_agents)
+    headers = {'User-Agent': user_agent}
+    response = requests.get(URL, headers=headers)
+
+    results = []
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Create a BeautifulSoup object with the response content
+        soup = BeautifulSoup(response.content, "html.parser")
+
+
+        # Find the name 
+        Name_results = soup.find_all("h1", class_="eM9Li2wbkQvvjxZB11sV mPudeIT67bJGOcOfKy92")
+        #adds name to results
+        for name in Name_results:
+            filtered_name = name.text.strip()
+            results.append(filtered_name)
+
+        # inside same class 
+        # Neigborhood, hour of operation ,cusine style , dining style , dress code, parking details , payment options,chef, phonenumber
+        Aditional_results = soup.find_all("p", class_="c_qirB1mFl5VRKHYJqTz")
+
+        ##ADDS results to array
+        for result in Aditional_results:
+            filtered_text = result.text.strip()
+            if filtered_text not in results:
+                results.append(filtered_text)
+        
+
     else:
         print("Failed to retrieve data from OpenTable.")
     
     return results
 
-result_list = scrape_opentable()
 
-# Print combined results
-for Name_results, Style_results, Price_results, review_results, review_number_results , filtered_hrefs_list in result_list:
-    print( "Name: ",Name_results.text.strip() ," Style: ", Style_results.text.strip())
-    print("PriceTag: ", Price_results.text.strip())
-    print("Latest Rev", review_results.text.strip(),"Number of revs: ", review_number_results.text.strip())
-    print("URL: ",filtered_hrefs_list)
-    print()
+
+
+
+
+user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+        'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36'
+    ]
+
+
+URL = 'https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTKyAiIDAyUdJWMUGaCACVDA0NzK2AAib6pkZaSjZAZSZahnaGxsaWxpqqNrYaZnYQBkmVoAlVgAJQNcg4L9_Rx9PKNcg-IDQ12DIoESlkAJZceyxMycxKScVLf8IpfMvLzUIr_8cqCkIdDm6FggDbQwLTGnOLUWAAfeno6nAAAA?originid=a8de24c8-54ed-4003-9800-342cfae1170b'
+URL_two ="https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=2"
+URL_three= "https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=3"
+URL_four = "https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=4"
+
+
+#Gets restaurnant URL LIST #change with url, url_2 ...
+Rest_URL_list = get_urls(URL, user_agents)
+
+
+#returns restaurant Information 
+result_list = scrape_opentable(Rest_URL_list[0],user_agents)
+
+
+for item in result_list:
+    print(item, "\n")
+
