7b67fc7adfb0a3a30cd9b5dad3393899d7f4bfd9
jpsanchezl10 <jp.sanchezlorenzo@icloud.com>
2023-06-08 13:21:36 -0500

    conection to db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import sqlite3
 
 
 def get_urls(URL, user_agents):
@@ -90,7 +91,7 @@ user_agents = [
     ]
 
 
-URL = 'https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTKyAiIDAyUdJWMUGaCACVDA0NzK2AAib6pkZaSjZAZSZahnaGxsaWxpqqNrYaZnYQBkmVoAlVgAJQNcg4L9_Rx9PKNcg-IDQ12DIoESlkAJZceyxMycxKScVLf8IpfMvLzUIr_8cqCkIdDm6FggDbQwLTGnOLUWAAfeno6nAAAA?originid=a8de24c8-54ed-4003-9800-342cfae1170b'
+URL = 'https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNbAIMTSzMja1MjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2MTI1NLHV0LMz0LA2NLU1OQBgugXIBrULC_n6OPZ5RrUHxgqGtQJFDCEiih7JSfn-2WX-RTmpecEZKfklgJlDAEWhodC6SBdqUl5hSn1gIA1zdSm6IAAAA=?originid=9d5b508d-dfd1-4592-8d2d-ee00e38a83ba'
 URL_two ="https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=2"
 URL_three= "https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=3"
 URL_four = "https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLWNTDTNTAPMTSzMjWwMjBQ0lEyRpEBCpgABQyNgJIQeVMlKyMdJTOQKkM9Q2NjS2NLUx1dCzM9CwMgy9QCqMQCKBngGhTs7-fo4xnlGhQfGOoaFAmUsARKKDvl52e75Rf5lOYlZ4TkpyRWAiUMgbZGxwJpoGVpiTnFqbUArHVfmqMAAAA=?originid=f3202d6c-9f7c-4e41-a42c-460ba4d0d007&corrid=3129ec52-0217-4054-83c3-600875857298&page=4"
@@ -99,13 +100,25 @@ URL_four = "https://www.opentable.com/lolz-view-all/H4sIAAAAAAAA_6tWMlKyUjIyMDLW
 #Gets restaurnant URL LIST #change with url, url_2 ...
 Rest_URL_list = get_urls(URL, user_agents)
 
-    
+result_list = []
+
 #returns restaurant Information 
 
 for url in Rest_URL_list:
-    result_list = scrape_opentable(url,user_agents)
+    results = scrape_opentable(url,user_agents)
+    result_list.append(results)
 
 
-for item in result_list:
-    print(item, "\n")
+number = 0
+#prints all the elements
+for i in result_list:
+    for item in result_list[number]:
+        print(item, "\n")
+    number = number + 1
+    print("next Item############################### \n")
+
+
+
+#print(result_list[0])
+
 
